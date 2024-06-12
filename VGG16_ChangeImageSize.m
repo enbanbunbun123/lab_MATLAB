023b018377b17a5d716c11099a60e16d2b4090db
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2024-06-12 13:35:31 +0900

    fix: 画像サイズを変更し保存する関数内での不要なコメントの削除

--- VGG16_ChangeImageSize.m
+++ VGG16_ChangeImageSize.m
@@ -8,12 +8,3 @@ imshow(ChangeImages)
     image(img1);
     imwrite(img1,str1);
         i=i+1;
-
-% fx1=figure(1);
-%     str1=append("Test_", int2str(i),".jpg");
-%     img1 = ChangeImages;
-%     image(img1);
-%     imwrite(img1,str1);
-%         i=i+1;
-
-       
